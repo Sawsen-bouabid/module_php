e17cf210790c2a60376d304ce1e3964cb1fdc95a
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-05-20 16:53:20 +0200

    ajout des restes détaits pour chaque produit

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -1,3 +1,40 @@
+
+
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta
+            name="description"
+            content="Le meilleur service de location d'avions privés en France. Confort, sécurité et commodité."
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- CSS only -->
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+            crossorigin="anonymous"
+    />
+
+    <!--<link href="css\style.css" rel="stylesheet" /> -->
+    <title>Location d'Avion Privé</title>
+    <link href="css\style.css" rel="stylesheet" />
+    <title>Location location d'Avion Privé</title>
+</head>
+
+<header>
+    <?php
+    include  'header.php';?>
+</header>
+
+
+
+<body>
+<h2> Catalogue Produits</h2>
+
+
 <?php
 $products = [
     "product_1" => [
@@ -41,17 +78,17 @@ $products = [
 
 
     </tr>
-          <tr>
-            <td> <?php
-                print_r($products['product_1']['Name']);  ?></td>
+    <tr>
+        <td> <?php
+            print_r($products['product_1']['Name']);  ?></td>
 
-            <td> <?php
-                print_r($products['product_2']['Name']);  ?> </td>
-            <td>
-                <?php
-                print_r($products['product_3']['Name']);  ?> </td>
+        <td> <?php
+            print_r($products['product_2']['Name']);  ?> </td>
+        <td>
+            <?php
+            print_r($products['product_3']['Name']);  ?> </td>
 
-          </tr>
+    </tr>
     <tr>
         <td> <?php
             print_r($products['product_1']['Model']);  ?></td>
@@ -85,5 +122,29 @@ $products = [
             print_r($products['product_3']['Discount']);  ?> </td>
 
     </tr>
-      </table>
+</table>
+
+
+</body>
+<footer> <?php
+    include 'footer.php';?> ; </footer>
+<script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossorigin="anonymous"
+></script>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
