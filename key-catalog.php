dc6bf27d2d62d8d7052a64efe52e1e9f2a7bfd22
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-05-20 14:39:44 +0200

    tableau associatif

---
+++ key-catalog.php
@@ -0,0 +1,28 @@
+
+
+<html>
+<header>
+
+</header>
+<body>
+<h1> Tableau Produits</h1>
+
+<?php
+$product_1 = [
+    "Name" => "Helicopter ",
+    "Model" => "H7524 Rouge",
+    "Price" => "500$/day",
+    "Discount" => "20%",
+    "Picture" => "https://th.bing.com/th/id/OIP.lU5_uQumNiu5th9kSVIaRwHaE8?pid=ImgDet&rs=1",
+];
+
+print_r($product_1);
+
+
+
+?>
+
+
+   </body>
+
+</html>
