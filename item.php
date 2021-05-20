bab347f829e9cfc8ef75bc7467f8fb82653ff138
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-05-20 09:47:59 +0200

    Ajout Titre et Prix

--- item.php
+++ item.php
@@ -1,7 +1,22 @@
-<?php $Article = "helicopter";
- $Price = "400 $ / day";
+
+<html>
+<header>
+
+</header>
+<body>
+<h1>Location Helicopter</h1>
+
+<?php
+ $Article = "Helicopter H7524 Rouge";
+ $Price = "Prix =500 $ / jour";
  $image = "https://th.bing.com/th/id/OIP.lU5_uQumNiu5th9kSVIaRwHaE8?pid=ImgDet&rs=1";
 
+echo "<img src=\"".$image."\" alt=\"\" width=\"600\">";
 
+echo "<h3>$Article</h3>";
+echo "<h3>$Price</h3>";
+  ?>
 
-echo "<img src=\"".$image."\" alt=\"\" width=\"200\">";
+</body>
+
+</html>
