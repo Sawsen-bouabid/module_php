0a3aaf4463b859ee410b73830f79e6cf65b77153
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-05-20 15:21:25 +0200

    commit

---
+++ footer.php
@@ -0,0 +1,64 @@
+
+<!-- new footer-->
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row rfooter gx-5">
+        <div class="col col-12 col-md">
+            <img class="mb-2" src="images\logopetit.png" alt="" />
+            <small class="d-block mb-3 text-muted" style="padding-left: 20px">
+                Copyright © LAP.fr™</small
+            >
+        </div>
+        <div class="col col-12 col-md">
+            <h5>Notre flotte</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="catalogue.html">Catalogue</a></li>
+                <li>
+                    <a class="text-muted" href="produit.html">Long-Courriers</a>
+                </li>
+                <li><a class="text-muted" href="produit.html">Jets</a></li>
+            </ul>
+        </div>
+        <div class="col col-12 col-md">
+            <h5>Nos Pilotes</h5>
+            <ul class="list-unstyled text-small">
+                <li>
+                    <a class="text-muted" href="cv_gabriella_boost.html">Gabriella</a>
+                </li>
+                <li>
+                    <a class="text-muted" href="cv_sawsen_boost.html">Sawsen</a>
+                </li>
+                <li>
+                    <a class="text-muted" href="cv_wissem_boots.html">Wissem</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col col-12 col-md">
+            <h5>Contact</h5>
+            <ul class="list-unstyled text-small">
+                <li>
+                    <a class="text-muted" href="contact.html"
+                    >Service Client en-ligne</a
+                    >
+                </li>
+                <li><a class="text-muted" href="#">+33(0)468529256</a></li>
+                <li><a class="text-muted" href="#">louer.avion@lap.fr</a></li>
+                <li><a class="text-muted" href="#"></a></li>
+            </ul>
+            <small class="d-block mb-3 text-muted">
+                <img
+                    src="images\icone_fb.png"
+                    alt="icone facebook"
+                    width="40px"
+                    height="40px" /><img
+                    src="images\icone-inst.png"
+                    alt="icone instagram"
+                    width="40px"
+                    height="40px" /><img
+                    src="images\icone_tw.png"
+                    alt="icone twitter"
+                    width="40px"
+                    height="40px"
+                /></small>
+        </div>
+    </div>
+</footer>
