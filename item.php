8cea0c5ecbd1e30d72d5fefe2db7b2b28983c45d
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-05-20 10:25:47 +0200

    Ajout du footer et du header

--- item.php
+++ item.php
@@ -1,7 +1,30 @@
 
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta
+            name="description"
+            content="Le meilleur service de location d'avions privés en France. Confort, sécurité et commodité."
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- CSS only -->
+    <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+            crossorigin="anonymous"
+    />
+
+    <!--<link href="css\style.css" rel="stylesheet" /> -->
+    <title>Location d'Avion Privé</title>
+    <link href="css\style.css" rel="stylesheet" />
+    <title>Location location d'Avion Privé</title>
+</head>
 <html>
 <header>
-
+    <?php
+  include 'header.php';?>
 </header>
 <body>
 <h1>Location Helicopter</h1>
@@ -13,10 +36,18 @@
 
 echo "<img src=\"".$image."\" alt=\"\" width=\"600\">";
 
-echo "<h3>$Article</h3>";
-echo "<h3>$Price</h3>";
+echo "<h2>$Article</h2>";
+echo "<h2>$Price</h2>";
   ?>
 
 </body>
-
+<footer> <?php
+    include 'footer.php';?> ; </footer>
+<script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossorigin="anonymous"
+></script>
+</body>
 </html>
+
