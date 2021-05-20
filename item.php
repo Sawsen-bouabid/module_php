b0e70ef340846fd3d92584a45d32e3a8af35841e
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-05-20 09:36:15 +0200

    ajout taille image

--- item.php
+++ item.php
@@ -1,8 +1,7 @@
 <?php $Article = "helicopter";
  $Price = "400 $ / day";
-header ("Content-type: image/jpeg");
-$image = "https://th.bing.com/th/id/OIP.lU5_uQumNiu5th9kSVIaRwHaE8?pid=ImgDet&rs=1";
+ $image = "https://th.bing.com/th/id/OIP.lU5_uQumNiu5th9kSVIaRwHaE8?pid=ImgDet&rs=1";
 
 
-echo $Article.$Price.'<imag scr ="'.$image.'" ; >'; ?>
 
+echo "<img src=\"".$image."\" alt=\"\" width=\"200\">";
