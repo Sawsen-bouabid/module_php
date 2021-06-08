04fc13a531115fb1dcff22e8db99fa1156f0aea0
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-06-08 10:27:51 +0200

    commit queries 1 et 2

--- database.php
+++ database.php
@@ -8,4 +8,32 @@ catch (Exception $e)
 {
     die('Erreur : ' . $e->getMessage());
 }
+
+
+// affichage produit :
+
+$query_1 = $bdd->query('SELECT `products`.`name`FROM `products`;
+');
+
+
+while ($donnees = $query_1->fetch())
+{
+    echo $donnees['name'] . '<br />';
+}
+
+$query_1->closeCursor();
+
+
+
+// Ajout d'une condition
+
+$query_2 = $bdd->query("SELECT `products`.`name`, `products`.`quantity`FROM `products`WHERE `products`.`quantity` = '0';");
+
+while ($donnees = $query_2->fetch())
+{
+echo $donnees['name'] . ' quantity is ' . $donnees['quantity'] . '<br>';
+}
+
+$query_2->closeCursor();
+
 ?>
