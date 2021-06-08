98d60823d3a063cb54beebe04d3aea034b0276e3
Sawsen-bouabid <sawsen.bouabid@le-campus-numerique.fr>
2021-06-08 15:32:01 +0200

    ajout des fonctions

--- database.php
+++ database.php
@@ -1,39 +1,81 @@
 
 <?php
-try
-{
-    $bdd = new PDO('mysql:host=localhost;port=3307;dbname=boutique_base;charset=utf8', 'sawsen', '$ousouta88');
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
 
+    $bdd= new PDO('mysql:host=localhost;port=3307;dbname=boutique_base;charset=utf8', 'sawsen', '$ousouta88');
 
-// affichage produit :
+function queries($queryToExecute) {
+    global $bdd;
+    $query = $bdd->query($queryToExecute);
 
-$query_1 = $bdd->query('SELECT `products`.`name`FROM `products`;
-');
+    return $query->fetchAll(PDO::FETCH_ASSOC);
 
-
-while ($donnees = $query_1->fetch())
-{
-    echo $donnees['name'] . '<br />';
 }
 
-$query_1->closeCursor();
 
 
 
-// Ajout d'une condition
 
-$query_2 = $bdd->query("SELECT `products`.`name`, `products`.`quantity`FROM `products`WHERE `products`.`quantity` = '0';");
+    function Product_list ()
+    {
 
-while ($donnees = $query_2->fetch())
+        $query_1 = "SELECT `products`.`name`FROM `products`";
+
+        return queries($query_1);
+
+    }
+
+
+
+
+
+function ProductQuantityNULL ()
 {
-echo $donnees['name'] . ' quantity is ' . $donnees['quantity'] . '<br>';
+
+    $query_3 ="SELECT `products`.`name`, `products`.`quantity`FROM `products`WHERE `products`.`quantity` ='0'";
+
+    return queries($query_3 );
+
+
 }
 
-$query_2->closeCursor();
+function OrderID1 ()
+{
+
+    $query_5 ="SELECT products.name, order_product.quantity, products.price 
+FROM products
+INNER JOIN order_product on products.id = order_product.product_id
+WHERE order_product.order_id = 1";
+
+    return queries($query_5 );
+
+
+}
+
+
+function order_list_by_custmors_name_charlize ()
+{
+
+    $query_9 ="SELECT orders.number, 
+ SUM(products.price * order_product.quantity) AS total_price,
+ customers.first_name
+FROM orders
+INNER JOIN order_product on orders.id = order_product.order_id
+INNER JOIN products on  products.id =order_product.product_id
+INNER JOIN customers on  customers.id =orders.customer_id
+
+WHERE customers.first_name = 'Charlize' 
+GROUP BY  orders.number ";
+
+    return queries($query_9 );
+
+
+}
+
+
+
 
 ?>
+
+
+
+
